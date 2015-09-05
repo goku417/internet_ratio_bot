e4ff2b0b44f7b0ca14c9f411377b7577ab2bebbe
Bunpa-Henri <b.henri.tan@gmail.com>
2015-09-05 19:35:35 -0400

    Add exception, gives random msg when status is duplicated

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import re
 import os
 import tweepy
 import time
+import random
 
 # Set Environment Variables into variable
 WEBSITE = os.environ.get('WEBSITE')
@@ -19,38 +20,48 @@ auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
 while 1:
-    page = requests.get(WEBSITE)
-    tree = html.fromstring(page.text)
+    try:
+        page = requests.get(WEBSITE)
+        tree = html.fromstring(page.text)
 
-    # Contains
-    month =  tree.xpath('//span[@class="txtdata"]/text()')
+        # Contains
+        month =  tree.xpath('//span[@class="txtdata"]/text()')
 
-    # Get the 6th element of the month element.
-    total_month_result = month[5]
-    msg = None
-    match = re.match(r'(\d+\.\d+)\sG',total_month_result)
-    total_month = match.group(1)
+        # Get the 6th element of the month element.
+        total_month_result = month[5]
+        msg = None
+        match = re.match(r'(\d+\.\d+)\sG',total_month_result)
+        total_month = match.group(1)
 
-    if float(total_month) < (150*0.10):
-        msg = "everything is ok, below 10%"
-    elif float(total_month) < (150*0.20):
-        msg = "everything is ok, below 20%"
-    elif float(total_month) < (150*0.30):
-        msg = "everything is ok, below 30%"
-    elif float(total_month) < (150*0.40):
-        msg = "everything is ok, below 10%"
-    elif float(total_month) < (150*0.50):
-        msg = "everything is still ok, below 50%"
-    elif float(total_month) < (150*0.60):
-        msg = "everything is still ok but you have to watch, below 60%"
-    elif float(total_month) < (150*0.70):
-        msg = "everything is still ok but check your internet limit, below 70%"
-    elif float(total_month) < (150*0.80):
-        msg = "You better see if you need a block, below 80%"
-    elif float(total_month) < (150*0.90):
-        msg = "YOU HAVE TO BUY A BLOCK, below 90% !!"
-    msg += " @goku417"
+        if float(total_month) < (150*0.10):
+            msg = "everything is ok, below 10%"
+        elif float(total_month) < (150*0.20):
+            msg = "everything is ok, below 20%"
+        elif float(total_month) < (150*0.30):
+            msg = "everything is ok, below 30%"
+        elif float(total_month) < (150*0.40):
+            msg = "everything is ok, below 10%"
+        elif float(total_month) < (150*0.50):
+            msg = "everything is still ok, below 50%"
+        elif float(total_month) < (150*0.60):
+            msg = "everything is still ok but you have to watch, below 60%"
+        elif float(total_month) < (150*0.70):
+            msg = "everything is still ok but check your internet limit, below 70%"
+        elif float(total_month) < (150*0.80):
+            msg = "You better see if you need a block, below 80%"
+        elif float(total_month) < (150*0.90):
+            msg = "YOU HAVE TO BUY A BLOCK, below 90% !!"
+        msg += " @goku417"
 
-    # Update status on bun's bot
-    api.update_status(status=msg)
+        # Update status on bun's bot
+        api.update_status(status=msg)
+    except:
+        msg = None
+        string_list = ['Everything seems like yesterday.', 'Yep same like yesterday.', 'Nothing change.',
+                       'You\'re not using your Internet :P', 'Same as yesterday.', 'Everything is fine.',
+                       'Everything is ok!', 'You can go check but i tell you, everything is fine']
+        number = random.randint(0, len(string_list) - 1)
+        msg = string_list[number] + ' @goku417'
+        api.update_status(status=msg)
+
     time.sleep(86400)
