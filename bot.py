0a087d83028f59e85c1ba60d7f484fb94cec6e3b
Bunpa-Henri <b.henri.tan@gmail.com>
2015-09-10 19:02:04 -0400

    Add feature to give the exact pourcentage of remaining ratio and show remaining ration in tweet.

--- bot.py
+++ bot.py
@@ -26,32 +26,23 @@ while 1:
 
         # Contains
         month =  tree.xpath('//span[@class="txtdata"]/text()')
+        available = tree.xpath('//div[@style="padding-top:3px;"]/text()')
 
         # Get the 6th element of the month element.
         total_month_result = month[5]
+        total_available_result = available[3]
         msg = None
-        match = re.match(r'(\d+\.\d+)\sG',total_month_result)
-        total_month = match.group(1)
+        match_month = re.match(r'(\d+\.\d+)\sG',total_month_result)
+        total_month = match_month.group(1)
+        match_available = re.match(r'\s(\d+\.\d+)\sG',total_available_result)
+        total_available = match_available.group(1)
 
-        if float(total_month) < (150*0.10):
-            msg = "everything is ok, below 10%"
-        elif float(total_month) < (150*0.20):
-            msg = "everything is ok, below 20%"
-        elif float(total_month) < (150*0.30):
-            msg = "everything is ok, below 30%"
-        elif float(total_month) < (150*0.40):
-            msg = "everything is ok, below 10%"
-        elif float(total_month) < (150*0.50):
-            msg = "everything is still ok, below 50%"
-        elif float(total_month) < (150*0.60):
-            msg = "everything is still ok but you have to watch, below 60%"
-        elif float(total_month) < (150*0.70):
-            msg = "everything is still ok but check your internet limit, below 70%"
-        elif float(total_month) < (150*0.80):
-            msg = "You better see if you need a block, below 80%"
-        elif float(total_month) < (150*0.90):
-            msg = "YOU HAVE TO BUY A BLOCK, below 90% !!"
-        msg += " @goku417"
+        if float(total_month) < float(140) and float(total_available) > float(10):
+            msg = "everything is ok, you're at " + str(float(total_month)/float(150)*100) + \
+                  "%. Available: "+ str(total_available) +"G. @goku417"
+        elif float(total_month) < float(150) and float(total_available) <= float(10):
+            msg = "This is critical, you're at " + str(float(total_month)/float(150)*100) + \
+                  "%. You have to buy a block now! @goku417"
 
         # Update status on bun's bot
         api.update_status(status=msg)
